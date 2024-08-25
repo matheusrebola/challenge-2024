fdc2f68d7e1e8426d86ac7735177a4c6d4202451
matheusrebola <m.cavalcantir@outlook.com>
2024-08-25 14:02:31 -0300

    feat: configuração das services

---
+++ orchestrator-service/src/main/java/challenge/vivo/orchestrator_service/core/service/OrchestrationService.java
@@ -0,0 +1,71 @@
+package challenge.vivo.orchestrator_service.core.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class OrchestrationService {
+
+    private final SagaOrchestratorProducer producer;
+    private final JsonUtil jsonUtil;
+    private final SagaExecutionController sagaExecutionController;
+
+    public void startSaga(Event event) {
+        event.setSource(ORCHESTRATOR);
+        event.setStatus(SUCCESS);
+        var topic = getTopic(event);
+        log.info("SAGA STARTED!");
+        addHistory(event, "Saga started!");
+        sendToProducerWithTopic(event, topic);
+    }
+
+    public void finishSagaSuccess(Event event) {
+        event.setSource(ORCHESTRATOR);
+        event.setStatus(SUCCESS);
+        log.info("SAGA FINISHED SUCCESSFULLY FOR EVENT {}!", event.getId());
+        addHistory(event, "Saga finished successfully!");
+        notifyFinishedSaga(event);
+    }
+
+    public void finishSagaFail(Event event) {
+        event.setSource(ORCHESTRATOR);
+        event.setStatus(FAIL);
+        log.info("SAGA FINISHED WITH ERRORS FOR EVENT {}!", event.getId());
+        addHistory(event, "Saga finished with errors!");
+        notifyFinishedSaga(event);
+    }
+
+    public void continueSaga(Event event) {
+        var topic = getTopic(event);
+        log.info("SAGA CONTINUING FOR EVENT {}", event.getId());
+        sendToProducerWithTopic(event, topic);
+    }
+
+    private ETopics getTopic(Event event) {
+        return sagaExecutionController.getNextTopic(event);
+    }
+
+    private void addHistory(Event event, String message) {
+        var history = History
+            .builder()
+            .source(event.getSource())
+            .status(event.getStatus())
+            .message(message)
+            .createdAt(LocalDateTime.now())
+            .build();
+        event.addToHistory(history);
+    }
+
+    private void sendToProducerWithTopic(Event event, ETopics topic) {
+        producer.sendEvent(jsonUtil.toJson(event), topic.getTopic());
+    }
+
+    private void notifyFinishedSaga(Event event) {
+        producer.sendEvent(jsonUtil.toJson(event), ETopics.NOTIFY_ENDING.getTopic());
+    }
+}
