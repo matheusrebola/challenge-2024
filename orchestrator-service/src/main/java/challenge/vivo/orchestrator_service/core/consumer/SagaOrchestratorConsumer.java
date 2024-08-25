8e1a6a2319a2f0e42e8d53ef84ced52a6c6e11aa
matheusrebola <m.cavalcantir@outlook.com>
2024-08-25 14:00:57 -0300

    feat: configuração dos consumers

---
+++ orchestrator-service/src/main/java/challenge/vivo/orchestrator_service/core/consumer/SagaOrchestratorConsumer.java
@@ -0,0 +1,56 @@
+package challenge.vivo.orchestrator_service.core.consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class SagaOrchestratorConsumer {
+    
+    private final OrchestrationService service;
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.start-saga}"
+    )
+    public void consumeStartSagaEvent(String payload) {
+        log.info("Receiving event {} from start-saga topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        service.startSaga(event);
+    }
+
+    @KafkaListener(
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.orchestrator}"
+    )
+    public void consumeOrchestratorEvent(String payload) {
+        log.info("Receiving event {} from orchestrator topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        service.continueSaga(event);
+    }
+
+    @KafkaListener(
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.finish-success}"
+    )
+    public void consumeFinishSagaSuccessEvent(String payload) {
+        log.info("Receiving event {} from finish-success topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        service.finishSagaSuccess(event);
+    }
+
+    @KafkaListener(
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.finish-fail}"
+    )
+    public void consumeFinishSagaFailEvent(String payload) {
+        log.info("Receiving event {} from finish-fail topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        service.finishSagaFail(event);
+    }
+}
