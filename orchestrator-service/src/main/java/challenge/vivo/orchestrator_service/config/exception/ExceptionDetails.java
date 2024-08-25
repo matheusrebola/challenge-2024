b2bbd66fbdcc2bdcee426ad3061d17cdf9ce2378
matheusrebola <m.cavalcantir@outlook.com>
2024-08-25 14:00:23 -0300

    feat: configuração das exceptions e do kafka

---
+++ orchestrator-service/src/main/java/challenge/vivo/orchestrator_service/config/exception/ExceptionDetails.java
@@ -0,0 +1,4 @@
+package challenge.vivo.orchestrator_service.config.exception;
+
+public record ExceptionDetails(int status, String message) {
+}
---
+++ orchestrator-service/src/main/java/challenge/vivo/orchestrator_service/config/exception/ValidationException.java
@@ -0,0 +1,12 @@
+package challenge.vivo.orchestrator_service.config.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message) {
+        super(message);
+    }
+    
+}
