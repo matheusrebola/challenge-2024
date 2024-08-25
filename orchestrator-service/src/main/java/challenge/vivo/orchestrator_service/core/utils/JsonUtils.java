9299ce00b664ff24e7c428fa573e707a36b24408
matheusrebola <m.cavalcantir@outlook.com>
2024-08-25 14:02:54 -0300

    feat: configuração do json

---
+++ orchestrator-service/src/main/java/challenge/vivo/orchestrator_service/core/utils/JsonUtils.java
@@ -0,0 +1,28 @@
+package challenge.vivo.orchestrator_service.core.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+
+public class JsonUtils {
+   private final ObjectMapper objectMapper;
+
+    public String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception ex) {
+            return "";
+        }
+    }
+
+    public Event toEvent(String json) {
+        try {
+            return objectMapper.readValue(json, Event.class);
+        } catch (Exception ex) {
+            return null;
+        }
+    } 
+}
