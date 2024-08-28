1f15c96509f5bca7c44a7f9f2fe96a9c690a18cc
matheusrebola <m.cavalcantir@outlook.com>
2024-08-28 16:20:25 -0300

    feat: configuração do consumidor de eventos do kafka

--- order-service/src/main/java/challenge/vivo/order_service/core/consumer/EventConsumer.java
+++ order-service/src/main/java/challenge/vivo/order_service/core/consumer/EventConsumer.java
@@ -1,5 +1,27 @@
 package challenge.vivo.order_service.core.consumer;
 
-public class EventConsumer {
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
+import challenge.vivo.order_service.core.service.EventService;
+import challenge.vivo.order_service.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class EventConsumer {
+	private final EventService service;
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.notify-ending}"
+    )
+    public void consumeNotifyEndingEvent(String payload) {
+        log.info("Receiving ending notification event {} from notify-ending topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        service.notifyEnding(event);
+    }
 }
