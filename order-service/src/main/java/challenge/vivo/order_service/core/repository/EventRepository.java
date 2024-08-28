bd96798b9099a9eacf31337ea85ec1d8ff6bb3b1
matheusrebola <m.cavalcantir@outlook.com>
2024-08-28 16:22:23 -0300

    configuração dos repositorios de evento e solicitação

--- order-service/src/main/java/challenge/vivo/order_service/core/repository/EventRepository.java
+++ order-service/src/main/java/challenge/vivo/order_service/core/repository/EventRepository.java
@@ -1,5 +1,18 @@
 package challenge.vivo.order_service.core.repository;
 
-public interface EventRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
 
+import challenge.vivo.order_service.core.document.Event;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface EventRepository extends MongoRepository<Event, String> {
+
+	List<Event> findAllByOrderByCreatedAtDesc();
+
+    Optional<Event> findTop1ByOrderIdOrderByCreatedAtDesc(String orderId);
+
+    Optional<Event> findTop1ByTransactionIdOrderByCreatedAtDesc(String transactionId);
 }
--- order-service/src/main/java/challenge/vivo/order_service/core/repository/OrderRepository.java
+++ order-service/src/main/java/challenge/vivo/order_service/core/repository/OrderRepository.java
@@ -1,5 +1,9 @@
 package challenge.vivo.order_service.core.repository;
 
-public interface OrderRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import challenge.vivo.order_service.core.document.Order;
+
+public interface OrderRepository extends MongoRepository<Order, String>  {
 
 }
