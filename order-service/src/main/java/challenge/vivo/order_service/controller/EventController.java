5c4d5da3328d828b19156bd4e1a1dada64b1d84d
matheusrebola <m.cavalcantir@outlook.com>
2024-08-28 16:18:15 -0300

    feat: configuração da controller de evento

--- order-service/src/main/java/challenge/vivo/order_service/controller/EventController.java
+++ order-service/src/main/java/challenge/vivo/order_service/controller/EventController.java
@@ -1,5 +1,30 @@
 package challenge.vivo.order_service.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import challenge.vivo.order_service.core.document.Event;
+import challenge.vivo.order_service.core.dto.EventFilters;
+import challenge.vivo.order_service.core.service.EventService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/event")
 public class EventController {
 
+	private final EventService service;
+
+    @GetMapping
+    public Event findByFilters(EventFilters filters) {
+        return service.findByFilters(filters);
+    }
+
+    @GetMapping("all")
+    public List<Event> findAll() {
+        return service.findAll();
+    }
 }
