8919ea91e78f7d697ffefd16c09e153134c8e788
matheusrebola <m.cavalcantir@outlook.com>
2024-08-30 11:50:45 -0300

    feat: microserviço vivo

---
+++ vivo-api/src/main/java/challenge/vivo/apivivo/ApiVivoApplication.java
@@ -0,0 +1,13 @@
+package challenge.vivo.apivivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVivoApplication.class, args);
+	}
+
+}
---
+++ vivo-api/src/test/java/challenge/vivo/apivivo/ApiVivoApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.vivo.apivivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
