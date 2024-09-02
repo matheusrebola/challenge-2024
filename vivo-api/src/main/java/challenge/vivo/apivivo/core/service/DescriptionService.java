718ff9f96a33b1d959f396ef7b323bda201bab3f
matheusrebola <m.cavalcantir@outlook.com>
2024-09-02 14:19:25 -0300

    feat: configuração da service

--- vivo-api/src/main/java/challenge/vivo/apivivo/core/service/DescriptionService.java
+++ vivo-api/src/main/java/challenge/vivo/apivivo/core/service/DescriptionService.java
@@ -1,5 +1,28 @@
 package challenge.vivo.apivivo.core.service;
 
+@Service
+@RequiredArgsConstructor
 public class DescriptionService {
 
+	protected final DescriptionRepository descriptionRepository;
+	
+	public List<Description> getAll(){
+		return descriptionRepository.findAll();
+	}
+	
+	public boolean exists(UUID id) {
+		return descriptionRepository.existsById(id);
+	}
+	
+	public Description findById(UUID id) {
+		return descriptionRepository.findById(id).orElse(null);
+	}
+	
+	public Description save(Description description) {
+		return descriptionRepository.save(description);
+	}
+	
+	public void delete(Description description) {
+		descriptionRepository.delete(description);
+	}
 }
