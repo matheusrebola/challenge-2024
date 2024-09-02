6bf25a78171ee9835b41dad095f7f7591a6b3dd2
matheusrebola <m.cavalcantir@outlook.com>
2024-09-02 14:10:10 -0300

    feat: configuração das mappers

--- vivo-api/src/main/java/challenge/vivo/apivivo/core/mapper/ProductMapper.java
+++ vivo-api/src/main/java/challenge/vivo/apivivo/core/mapper/ProductMapper.java
@@ -1,5 +1,22 @@
 package challenge.vivo.apivivo.core.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class ProductMapper {
 
+	private final ModelMapper modelMapper;
+	
+	public Product map(ProductCreateDto dto) {
+		Product acesso = modelMapper.map(dto, Product.class);
+		return acesso;
+	}
+	
+	public ProductDto map(Product acesso) {
+		ProductDto dto = modelMapper.map(acesso, ProductDto.class);
+		return dto;
+	}
 }
