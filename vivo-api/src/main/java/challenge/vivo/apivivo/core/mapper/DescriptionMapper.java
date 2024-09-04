90e2e59ec48e7ab56e05c901b5c89106ca64655a
matheusrebola <m.cavalcantir@outlook.com>
2024-09-04 13:08:19 -0300

    feat: configuração das mappers

--- vivo-api/src/main/java/challenge/vivo/apivivo/core/mapper/DescriptionMapper.java
+++ vivo-api/src/main/java/challenge/vivo/apivivo/core/mapper/DescriptionMapper.java
@@ -1,5 +1,23 @@
 package challenge.vivo.apivivo.core.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class DescriptionMapper {
 
+	private final ModelMapper modelMapper;
+
+	public Description map(DescriptionCreateDTO dto) {
+		Description description = modelMapper.map(dto, Description.class);
+		description.setDataCadastro(Instant.now());
+		return description;
+	}
+
+	public DescriptionDTO map(Description description) {
+		DescriptionDTO dto = modelMapper.map(cliente, DescriptionDTO.class);
+		return dto;
+	}
 }
--- vivo-api/src/main/java/challenge/vivo/apivivo/core/mapper/PriceMapper.java
+++ vivo-api/src/main/java/challenge/vivo/apivivo/core/mapper/PriceMapper.java
@@ -1,5 +1,17 @@
 package challenge.vivo.apivivo.core.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class PriceMapper {
 
+	private final ModelMapper modelMapper;
+
+	public PriceDTO map(Price price) {
+		PriceDTO dto = modelMapper.map(cliente, PriceDTO.class);
+		return dto;
+	}
 }
