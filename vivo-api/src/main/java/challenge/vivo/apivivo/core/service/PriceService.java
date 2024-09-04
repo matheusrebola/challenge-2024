eb42c5b68d7a0bf028d766c8b61c23f75a232b1e
matheusrebola <m.cavalcantir@outlook.com>
2024-09-04 13:08:53 -0300

    feat: configuração das services

--- vivo-api/src/main/java/challenge/vivo/apivivo/core/service/PriceService.java
+++ vivo-api/src/main/java/challenge/vivo/apivivo/core/service/PriceService.java
@@ -1,5 +1,21 @@
 package challenge.vivo.apivivo.core.service;
 
+@Service
+@RequiredArgsConstructor
 public class PriceService {
+
+	protected final PriceRepository priceRepository;
+	
+	public List<Price> getAll(){
+		return priceRepository.findAll();
+	}
+	
+	public boolean exists(UUID id) {
+		return priceRepository.existsById(id);
+	}
+	
+	public Price findById(UUID id) {
+		return priceRepository.findById(id).orElse(null);
+	}
 
 }
--- vivo-api/src/main/java/challenge/vivo/apivivo/core/service/ProductService.java
+++ vivo-api/src/main/java/challenge/vivo/apivivo/core/service/ProductService.java
@@ -1,5 +1,28 @@
 package challenge.vivo.apivivo.core.service;
 
+@Service
+@RequiredArgsConstructor
 public class ProductService {
 
+	protected final ProductRepository productRepository;
+	
+	public List<Product> getAll(){
+		return productRepository.findAll();
+	}
+	
+	public boolean exists(UUID id) {
+		return productRepository.existsById(id);
+	}
+	
+	public Product findById(UUID id) {
+		return productRepository.findById(id).orElse(null);
+	}
+	
+	public Product save(Product product) {
+		return productRepository.save(product);
+	}
+	
+	public void delete(Product product) {
+		productRepository.delete(product);
+	}
 }
