97ff1c047d335b9ccd8d5ba6cbe06320e525c832
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-17 22:15:56 -0300

    feat: término das services

--- apivivo/src/main/java/challenge/vivo/apivivo/core/service/PriceService.java
+++ apivivo/src/main/java/challenge/vivo/apivivo/core/service/PriceService.java
@@ -1,8 +1,28 @@
 package challenge.vivo.apivivo.core.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
-@Service
-public class PriceService {
+import challenge.vivo.apivivo.core.entities.Price;
+import challenge.vivo.apivivo.core.repository.PriceRepository;
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
+public class PriceService {
+    private final PriceRepository priceRepository;
+
+    public List<Price> findAll(){
+        return priceRepository.findAll();
+    }
+
+    public Price findById(UUID id){
+        return priceRepository.findById(id).orElse(null);
+    }
+
+    public Price savPrice(Price price){
+        return priceRepository.save(price);
+    }
 }
--- apivivo/src/main/java/challenge/vivo/apivivo/core/service/ProductService.java
+++ apivivo/src/main/java/challenge/vivo/apivivo/core/service/ProductService.java
@@ -1,8 +1,28 @@
 package challenge.vivo.apivivo.core.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
-@Service
-public class ProductService {
+import challenge.vivo.apivivo.core.entities.Product;
+import challenge.vivo.apivivo.core.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    public Product findById(UUID id){
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product saveProduct(Product product){
+        return productRepository.save(product);
+    }
 }
