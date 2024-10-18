99f3728168583b5cb6ada9c120849dcc678214d4
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-17 22:05:18 -0300

    feat: configuração das repositorys

--- apivivo/src/main/java/challenge/vivo/apivivo/core/repository/DescriptionRepository.java
+++ apivivo/src/main/java/challenge/vivo/apivivo/core/repository/DescriptionRepository.java
@@ -1,6 +1,10 @@
 package challenge.vivo.apivivo.core.repository;
 
 import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import challenge.vivo.apivivo.core.entities.Description;
 
 @Repository
--- apivivo/src/main/java/challenge/vivo/apivivo/core/repository/ProductRepository.java
+++ apivivo/src/main/java/challenge/vivo/apivivo/core/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package challenge.vivo.apivivo.core.repository;
 
 import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import challenge.vivo.apivivo.core.entities.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
 }
