7a40d2e8d6af2ab9b6adf77b07630ac952054444
matheusrebola <m.cavalcantir@outlook.com>
2024-10-18 11:06:37 -0300

    feat: alteração do ID de string para UUID

--- apivivo/src/main/java/challenge/vivo/apivivo/core/entities/Description.java
+++ apivivo/src/main/java/challenge/vivo/apivivo/core/entities/Description.java
@@ -1,14 +1,14 @@
 package challenge.vivo.apivivo.core.entities;
 
-import java.util.Locale.Category;
 import java.util.UUID;
 
+import challenge.vivo.apivivo.core.entities.enums.Category;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
 public record Description(
-     @Id UUID id,
+    @Id UUID id,
     String text,
     String url,
     Category category    
