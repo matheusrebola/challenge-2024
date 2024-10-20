ef2ff57fc4e0da3f13945b3ff552a3ce3551098a
matheusrebola <m.cavalcantir@outlook.com>
2024-10-20 18:57:25 -0300

    feat: redução dos microserviços

---
+++ change-data-capture-service/src/main/java/challenge/vivo/changedatacapture/ChangeDataCaptureApplication.java
@@ -0,0 +1,13 @@
+package challenge.vivo.descriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeDataCaptureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeDataCaptureApplication.class, args);
+	}
+
+}
---
+++ vivo-service/src/main/java/challenge/vivo/vivoservice/VivoServiceApplication.java
@@ -0,0 +1,13 @@
+package challenge.vivo.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivoServiceApplication.class, args);
+	}
+
+}
