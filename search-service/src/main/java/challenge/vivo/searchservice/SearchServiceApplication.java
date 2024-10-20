f735a77a412090ceb29a85d7f70e57c9d2e237ff
matheusrebola <m.cavalcantir@outlook.com>
2024-10-20 20:27:52 -0300

    feat: acréscimo dos arquivos necessários

---
+++ search-service/src/main/java/challenge/vivo/searchservice/SearchServiceApplication.java
@@ -0,0 +1,13 @@
+package challenge.vivo.searchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServiceApplication.class, args);
+	}
+
+}
