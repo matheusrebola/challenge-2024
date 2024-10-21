0f805c9069bf38e7f2ac02e4bf54d597c2f68177
matheusrebola <m.cavalcantir@outlook.com>
2024-10-20 21:37:06 -0300

    feat: altualização do nome dos microserviços

--- start_containers.py
+++ start_containers.py
@@ -4,10 +4,10 @@ import subprocess
 # Diretórios com os arquivos docker-compose.yml
 SERVICES = [
     ".",
-    "order-service",
-    "orchestrator-service",
-    "product-service",
-    "chnage-data-capture-service"
+    "order-service"
+    "orchestrator-service"
+    "item-service"
+    "search-service"
 ]
 
 def is_container_running(service_name):
