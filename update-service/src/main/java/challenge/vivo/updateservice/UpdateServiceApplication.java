c5c786bf20de0dba44322fcd8f0195da63dcfd17
matheusrebola <m.cavalcantir@outlook.com>
2024-10-21 10:41:15 -0300

    feat: criação do novo microserviço

---
+++ update-service/src/main/java/challenge/vivo/updateservice/UpdateServiceApplication.java
@@ -0,0 +1,13 @@
+package challenge.vivo.updateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateServiceApplication.class, args);
+	}
+
+}
---
+++ update-service/src/test/java/challenge/vivo/updateservice/UpdateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.vivo.updateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
