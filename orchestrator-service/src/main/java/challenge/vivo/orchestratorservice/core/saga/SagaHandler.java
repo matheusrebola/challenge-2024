8d72361a21cea5b2f01caf13db8063af4d0a584e
matheusrebola <m.cavalcantir@outlook.com>
2024-10-21 10:05:07 -0300

    feat: adição do não encontrado ao item

--- orchestrator-service/src/main/java/challenge/vivo/orchestratorservice/core/saga/SagaHandler.java
+++ orchestrator-service/src/main/java/challenge/vivo/orchestratorservice/core/saga/SagaHandler.java
@@ -1,8 +1,20 @@
 package challenge.vivo.orchestratorservice.core.saga;
 
-import static challenge.vivo.orchestratorservice.core.enums.EEventSource.*;
-import static challenge.vivo.orchestratorservice.core.enums.ESagaStatus.*;
-import static challenge.vivo.orchestratorservice.core.enums.ETopics.*;
+import static challenge.vivo.orchestratorservice.core.enums.EEventSource.ITEM_SERVICE;
+import static challenge.vivo.orchestratorservice.core.enums.EEventSource.ORCHESTRATOR;
+import static challenge.vivo.orchestratorservice.core.enums.EEventSource.SEARCH_SERVICE;
+import static challenge.vivo.orchestratorservice.core.enums.EEventSource.UPDATE_SERVICE;
+import static challenge.vivo.orchestratorservice.core.enums.ESagaStatus.FAIL;
+import static challenge.vivo.orchestratorservice.core.enums.ESagaStatus.NOT_FOUND;
+import static challenge.vivo.orchestratorservice.core.enums.ESagaStatus.ROLLBACK_PENDING;
+import static challenge.vivo.orchestratorservice.core.enums.ESagaStatus.SUCCESS;
+import static challenge.vivo.orchestratorservice.core.enums.ETopics.FINISH_FAIL;
+import static challenge.vivo.orchestratorservice.core.enums.ETopics.FINISH_SUCCESS;
+import static challenge.vivo.orchestratorservice.core.enums.ETopics.ITEM_FAIL;
+import static challenge.vivo.orchestratorservice.core.enums.ETopics.ITEM_SUCCESS;
+import static challenge.vivo.orchestratorservice.core.enums.ETopics.SEARCH_FAIL;
+import static challenge.vivo.orchestratorservice.core.enums.ETopics.UPDATE_FAIL;
+import static challenge.vivo.orchestratorservice.core.enums.ETopics.UPDATE_SUCCESS;
 
 public class SagaHandler {
   private SagaHandler() {
@@ -13,9 +25,10 @@ public class SagaHandler {
       { ORCHESTRATOR, SUCCESS, ITEM_SUCCESS },
       { ORCHESTRATOR, FAIL, FINISH_FAIL },
 
+      { ITEM_SERVICE, NOT_FOUND, SEARCH_SERVICE },
       { ITEM_SERVICE, ROLLBACK_PENDING, ITEM_FAIL },
       { ITEM_SERVICE, FAIL, FINISH_FAIL },
-      { ITEM_SERVICE, SUCCESS, SEARCH_SUCCESS },
+      { ITEM_SERVICE, SUCCESS, FINISH_SUCCESS },
 
       { SEARCH_SERVICE, ROLLBACK_PENDING, SEARCH_FAIL },
       { SEARCH_SERVICE, FAIL, ITEM_FAIL },
