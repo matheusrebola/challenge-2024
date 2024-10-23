dc777b76c1e880bd8f67c6df6a974b607dadb092
matheusrebola <matheusrebola@outlook.com>
2024-10-23 16:03:39 -0300

    criação das entidades para garantir a busca

--- search-service/src/main/java/challenge/vivo/searchservice/core/document/Product.java
+++ search-service/src/main/java/challenge/vivo/searchservice/core/document/Product.java
@@ -1,8 +1,13 @@
 package challenge.vivo.searchservice.core.document;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-public record Product() {
+public record Product(
+  @Id String id,
+  String productCode,
+  Integer available
+) {
 
 }
--- search-service/src/main/java/challenge/vivo/searchservice/core/document/SearchProduct.java
+++ search-service/src/main/java/challenge/vivo/searchservice/core/document/SearchProduct.java
@@ -1,8 +1,33 @@
 package challenge.vivo.searchservice.core.document;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
-public class SearchProduct {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-}
+@Document
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchProduct{
+  @Id String id;
+  private Product product;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+  
+  public void prePersist() {
+    var now = LocalDateTime.now();
+    createdAt = now;
+    updatedAt = now;
+  }
+
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+} 
